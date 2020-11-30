09997885cf2294d3473adbc6777bae5190755759
monazakria <monazakria2015@gmail.com>
2020-11-30 22:40:25 +0200

    Fix register payment

--- sales_measurement/models/account_move.py
+++ sales_measurement/models/account_move.py
@@ -6,17 +6,19 @@ from odoo import fields, models, api
 class AccountMoveInherit(models.Model):
     _inherit = 'account.move'
 
-    def _post(self, soft):
-        result = super(AccountMoveInherit, self)._post(soft)
+    def _post(self, soft=True):
+        # OVERRIDE
+        # Auto-reconcile the invoice with payments coming from sale payments.
+        posted = super()._post(soft)
 
-        for invoice in self:
-
+        for invoice in posted.filtered(lambda move: move.is_invoice()):
             if not invoice.amount_residual:
                 continue
             sale_line_ids = self.invoice_line_ids.mapped("sale_line_ids")
             if sale_line_ids:
 
-                payments = self.env['account.payment'].search([('sale_order_id', 'in', sale_line_ids.mapped("order_id").ids)])
+                payments = self.env['account.payment'].search(
+                    [('sale_order_id', 'in', sale_line_ids.mapped("order_id").ids)])
 
                 for payment in payments:
                     if not invoice.amount_residual:
@@ -26,4 +28,4 @@ class AccountMoveInherit(models.Model):
                     )
                     if credit_line:
                         invoice.js_assign_outstanding_line(credit_line.id)
-        return result
+        return posted
