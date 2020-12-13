3a7c99c812dfb0ef1e066099355906b7c7951370
monazakria <monazakria2015@gmail.com>
2020-12-14 00:13:12 +0200

    [WIP] import_product_bom

---
+++ import_product_bom/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Part of BrowseInfo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'Import Product Bill Of Material from Excel or CSV File',
+    'version': '14.0.0.0',
+    'category': 'Sales',
+    'depends': ['sale_mrp'],
+    'data': [
+                'security/ir.model.access.csv',
+                'views/sale_view.xml',
+            ],
+    'demo': [],
+    'test': [],
+    'installable':True,
+    'auto_install':False,
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
---
+++ import_product_bom/models/import_product_bom.py
@@ -0,0 +1,185 @@
+# -*- coding: utf-8 -*-
+
+from odoo import api, fields, models, _
+from odoo.exceptions import Warning, ValidationError
+import binascii
+import tempfile
+import logging
+
+_logger = logging.getLogger(__name__)
+import io
+
+try:
+    import xlrd
+except ImportError:
+    _logger.debug('Cannot `import xlrd`.')
+try:
+    import csv
+except ImportError:
+    _logger.debug('Cannot `import csv`.')
+try:
+    import base64
+except ImportError:
+    _logger.debug('Cannot `import base64`.')
+
+
+class ProductBillwizard(models.TransientModel):
+    _name = 'product.bom.wizard'
+    _description = "Product Bill of Material Wizard"
+
+    import_file = fields.Binary(string="Select File")
+    import_option = fields.Selection([('csv', 'CSV File'), ('xls', 'XLS File')], string='Select', default='csv')
+
+    def import_bom(self):
+        res = False
+        if self.import_option == 'xls':
+            try:
+                fp = tempfile.NamedTemporaryFile(delete=False, suffix=".xlsx")
+                fp.write(binascii.a2b_base64(self.import_file))
+
+                fp.seek(0)
+                values = {}
+                workbook = xlrd.open_workbook(fp.name)
+                sheet = workbook.sheet_by_index(0)
+
+            except Exception:
+                raise ValidationError(_("Please select any file or You have selected invalid file"))
+            all_bom_val = {}
+
+            current_product = False
+            for row_no in range(sheet.nrows):
+
+                if row_no <= 0:
+                    fields = map(lambda row: row.value.encode('utf-8'), sheet.row(row_no))
+
+                else:
+                    line = list(map(lambda row: isinstance(row.value, bytes) and row.value.encode('utf-8') or str(row.value),
+                            sheet.row(row_no)))
+                    component = self.env['product.product'].sudo().search([('name', '=', line[7])])
+                    component_uom_id = self.env['uom.uom'].sudo().search([('name', '=', line[9])])
+
+                    if line[0]:
+
+                        current_product = product_tmp = self.env['product.template'].sudo().search([('name', '=', line[0])])
+                        product = self.env['product.product'].sudo().search([('name', '=', line[1])])
+                        product_uom_id = self.env['uom.uom'].sudo().search([('name', '=', line[3])])
+                        company = self.env['res.company'].sudo().search([('name', '=', line[6])])
+                        if line[5] == 'Kit':
+                            bom_type = 'phantom'
+
+                        elif line[5] == 'Manufacture this product':
+                            bom_type = 'normal'
+                        else:
+                            bom_type = False
+                        bom_vals = {
+                                'product_tmpl_id': product_tmp.id,
+                                'product_id': product.id,
+                                'product_qty': line[2],
+                                'product_uom_id': product_uom_id.id,
+                                'code': line[4],
+                                'type': bom_type,
+                                'company_id': company.id,
+                                'bom_line_ids': [(0,0, {
+                                    'product_id': component.id,
+                                    'product_qty': line[8],
+                                    'product_uom_id': component_uom_id.id
+                                })]
+                                    }
+                        all_bom_val[product_tmp.id] = bom_vals
+                    else:
+                        components = all_bom_val.get(current_product.id).get('bom_line_ids')
+                        components.append((0, 0, {
+                                    'product_id': component.id,
+                                    'product_qty': line[8],
+                                    'product_uom_id': component_uom_id.id
+                                }))
+
+            if all_bom_val:
+                res = self.update_order_line_bom(all_bom_val)
+
+        else:
+            keys = ['product', 'product variant', 'quantity', 'uom', 'reference', 'bom type', 'company',
+                    'component name', 'component quantity', 'component uom']
+            try:
+                csv_data = base64.b64decode(self.import_file)
+                data_file = io.StringIO(csv_data.decode("utf-8"))
+
+                data_file.seek(0)
+                file_reader = []
+                csv_reader = csv.reader(data_file, delimiter=',')
+
+                file_reader.extend(csv_reader)
+
+            except Exception:
+                raise ValidationError(_("Please select any file or You have selected invalid file"))
+
+            all_bom_val = {}
+            current_product = False
+            for i in range(len(file_reader)):
+                field = list(map(str, file_reader[i]))
+                values = dict(zip(keys, field))
+
+                if values:
+                    if i == 0:
+                        continue
+                    else:
+                        component = self.env['product.product'].sudo().search([('name', '=', field[7])])
+                        component_uom_id = self.env['uom.uom'].sudo().search([('name', '=', field[9])])
+                        if field[0]:
+
+                            current_product = product_tmp = self.env['product.template'].sudo().search(
+                                [('name', '=', field[0])])
+                            product = self.env['product.product'].sudo().search([('name', '=', field[1])])
+                            product_uom_id = self.env['uom.uom'].sudo().search([('name', '=', field[3])])
+                            company = self.env['res.company'].sudo().search([('name', '=', field[6])])
+                            if field[5] == 'Kit':
+                                bom_type = 'phantom'
+
+                            elif field[5] == 'Manufacture this product':
+                                bom_type = 'normal'
+                            else:
+                                bom_type = False
+                            bom_vals = {
+                                'product_tmpl_id': product_tmp.id,
+                                'product_id': product.id,
+                                'product_qty': field[2],
+                                'product_uom_id': product_uom_id.id,
+                                'code': field[4],
+                                'type': bom_type,
+                                'company_id': company.id,
+                                'bom_line_ids': [(0, 0, {
+                                    'product_id': component.id,
+                                    'product_qty': field[8],
+                                    'product_uom_id': component_uom_id.id
+                                })]
+                            }
+                            all_bom_val[product_tmp.id] = bom_vals
+                        else:
+                            components = all_bom_val.get(current_product.id).get('bom_line_ids')
+                            components.append((0, 0, {
+                                'product_id': component.id,
+                                'product_qty': field[8],
+                                'product_uom_id': component_uom_id.id
+                            }))
+
+
+            if all_bom_val:
+                res = self.update_order_line_bom(all_bom_val)
+
+        return res
+
+    def update_order_line_bom(self, all_bom_val):
+        bom_vales = all_bom_val.values()
+        for bom_val in bom_vales:
+            product_tmp = self.env['product.template'].sudo().browse(bom_val.get("product_tmpl_id"))
+
+            if not product_tmp.bom_ids:
+                bom_id = self.env['mrp.bom'].sudo().create(bom_val)
+            else:
+                vals_to_be_updated = {}
+                for key in bom_val.keys():
+                    if bom_val.get(key) and key != 'product_tmpl_id':
+                        vals_to_be_updated[key] = bom_val.get(key)
+
+                product_tmp.bom_ids.sudo().write(vals_to_be_updated)
+        return True
